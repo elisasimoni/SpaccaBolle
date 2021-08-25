eb8bc0937d155ae1438c71564298dfdb79320d47
Elisa Simoni <73821477+xeli00@users.noreply.github.com>
2021-08-25 17:42:28 +0200

    Add fixed handler, Launcher

--- src/dev/spaccabolle/Launcher.java
+++ src/dev/spaccabolle/Launcher.java
@@ -1,10 +1,21 @@
 package dev.spaccabolle;
 
+/**
+ * The Class Launcher.
+ */
 public class Launcher {
     
+    /** The Constant GAME_WIDTH. */
     public static final int GAME_WIDTH = 840;
+    
+    /** The Constant GAME_HEIGHT. */
     public static final  int GAME_HEIGHT = 740;
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args){
 		Game game = new Game("SPACCABOLLE", GAME_WIDTH, GAME_HEIGHT);
 		game.start();
