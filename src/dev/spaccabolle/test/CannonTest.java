36ed89c15c2eff1213d0a5f9a5bbe03dbb0ed139
Michele Nardini <michele.nardini3g@gmail.com>
2021-08-25 18:22:26 +0200

    Update CannonTest.java

--- src/dev/spaccabolle/test/CannonTest.java
+++ src/dev/spaccabolle/test/CannonTest.java
@@ -4,12 +4,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import dev.spaccabolle.entity.Ball;
 import dev.spaccabolle.entity.Cannon;
+import dev.spaccabolle.entity.CollectBall;
 
 class CannonTest {
 	
-	@Test
+        int x = 1000;
+        int y = 10;
+        int height = 20;
+        int width = 20;
+        CollectBall collectBall = new CollectBall();
+        Cannon cannon = new Cannon(x,y,height, width, collectBall);
+    
+        @Test
 	void testGetCollectBall() {
-		assertEquals(Cannon.collectBall, Cannon.getCollectBall());
+		assertEquals(collectBall, cannon.getCollectBall());
 	}
+        
+        @Test
+        void testGetNumBall() {
+            cannon.getCollectBall().addBall(new Ball(0,0,0,0,0,0));
+            assertEquals(2,cannon.getCollectBall().numBolle());
+            cannon.getCollectBall().addBall(new Ball(1,1,1,1,1,1));
+            assertEquals(3,cannon.getCollectBall().numBolle());
+        }
+        
+        @Test
+        void testSpeedCannon() {
+            cannon.setSpeed(30);
+            assertEquals(30,cannon.getSpeed());
+        }
+        
+        @Test
+        void testCheckBounce() {
+            cannon.checkBounce();
+            assertEquals(false,cannon.bounce);
+        }
 }
+
